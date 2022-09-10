884d2e9e7e5bfebed97be83365a0be39da154abd
MarioJr92 <mariorodjr92@gmail.com>
2022-09-10 08:05:12 -0400

    created mailer for tweets

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'mariorodjr92@gmail.com'
   layout 'mailer'
 end
---
+++ app/mailers/tweet_mailer.rb
@@ -0,0 +1,2 @@
+class TweetMailer < ApplicationMailer
+end
